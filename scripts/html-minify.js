bfcd0d42bf4cb00f847d1b007874ca92f269b02c
Zack Tollman <tollmanz@gmail.com>
2019-01-21 16:58:44 -0600

    Attempt HTML minification

---
+++ scripts/html-minify.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const glob = require('glob');
+const { minify } = require('html-minifier');
+
+(async() => {
+  try {
+    const files = await new Promise((resolve, reject) => {
+      glob('./public/**/*.html', (err, files) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(files);
+      });
+    });
+
+    files.map(file => {
+      const contents = fs.readFileSync(file).toString();
+      const minified = minify(contents, {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        minifyJS: true,
+        minifyCSS: true
+      });
+      fs.writeFileSync(file, minified);
+    });
+  } catch(e) {
+    console.error(e);
+  }
+})();
+
