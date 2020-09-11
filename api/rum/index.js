9b50b0b9399ba8e34f71fd2e5fd96af153a7d84b
Zack Tollman <tollmanz@gmail.com>
2020-09-10 19:33:21 -0500

    Emit first event

--- api/rum/index.js
+++ api/rum/index.js
@@ -1,3 +1,9 @@
+const honeycomb = require('honeycomb-beeline')({
+  writeKey: process.env.HONEYCOMB_WRITE_KEY,
+  dataset: 'www.tollmanz.com',
+  serviceName: 'rum'
+});
+
 module.exports = (req, res) => {
   res.json({
     hello: 'world'
