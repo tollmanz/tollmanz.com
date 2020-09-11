bb89ccaac63e136f5c0359886e24bf0464d824d1
Zack Tollman <tollmanz@gmail.com>
2020-09-10 19:38:09 -0500

    Add first trace

--- api/rum/index.js
+++ api/rum/index.js
@@ -1,10 +1,15 @@
-const honeycomb = require('honeycomb-beeline')({
+const beeline = require('honeycomb-beeline')({
   writeKey: process.env.HONEYCOMB_WRITE_KEY,
   dataset: 'www.tollmanz.com',
   serviceName: 'rum'
 });
 
 module.exports = (req, res) => {
+  const trace = beeline.startTrace({
+    foo: 'bar'
+  });
+  beeline.finishTrace(trace);
+
   res.json({
     hello: 'world'
   });
